3e785c68e5a6e8b076abbe4f72a83ad55bd62cd1
itstomis <thomas@watanabe.ca>
2012-12-05 12:16:14 -0800

    Update Powerpack_Controller/Motor_Controller.h

--- Powerpack_Controller/Motor_Controller.h
+++ Powerpack_Controller/Motor_Controller.h
@@ -2,9 +2,27 @@
 #define __MOTOR_CONTROLLER_H_
 
 #include <Arduino.h>
+#include <PID_v1.h>
+
+class MotorController
+{
+  
+  public:
+  
+    //Constants
+  
+  
+    //Functions
+  
+  
+  
+    //Simple Display Functions
+  
+  
+  
+  private:
+  
 
 
-
-
-
+};
 #endif __MOTOR_CONTROLLER_H_
