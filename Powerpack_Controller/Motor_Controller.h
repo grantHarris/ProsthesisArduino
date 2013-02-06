29b5881e82e991d3790a25e3d236c7ba42ad6ac4
itstomis <thomas@watanabe.ca>
2013-02-06 10:34:19 -0800

    Update Powerpack_Controller/Motor_Controller.h

--- Powerpack_Controller/Motor_Controller.h
+++ Powerpack_Controller/Motor_Controller.h
@@ -34,7 +34,7 @@ class MotorController
     float mOutput;
     float mSetpoint;
     
-    PID mPIDControl;
+    PID mPIDControl(&mInput, &mOutput, &mSetpoint, mKp, mKi, mKd, DIRECT);
 
 };
 #endif __MOTOR_CONTROLLER_H_
