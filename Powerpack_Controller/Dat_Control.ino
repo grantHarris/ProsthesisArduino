8645bcb15be6b7d0b857ab3f561ce40203989775
itstomis <thomas@watanabe.ca>
2013-02-06 09:43:04 -0800

    Create Dat_Control.ino

---
+++ Powerpack_Controller/Dat_Control.ino
@@ -0,0 +1,24 @@
+/*
+Basic Controlling Code
+November 23 2012
+Thomas Watanabe
+Acknowledgements: PID Library by Brett Beauregard
+*/
+
+#include <Motor_Controller.h>
+
+MotorController MyController(0, 10, 2, 3, 4, 5, 0, 0, 0, 10, 0, 0, 0);
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("PID Code");
+
+  delay(2000);
+
+  MyController.Initialize();
+}
+
+void loop() { //will replace loop() with more robust 10Hz interrupt later
+  MyController.Iterate();
+}
