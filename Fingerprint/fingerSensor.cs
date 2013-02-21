45ed96e8fae919cbe5a324abc3f91d6b3aab7fc4
Francesco Anzovino <francescoanzo@gmail.com>
2013-02-21 14:53:50 -0800

    added synchronous serial read

--- Fingerprint/fingerSensor.cs
+++ Fingerprint/fingerSensor.cs
@@ -6,34 +6,55 @@ using System.IO.Ports;
 
 namespace Fingerprint
 {
-    public class fingerSensor
+    public class FingerSensor
     {
-        uint HEADER = 0xEF01;
+        ushort HEADER = 0xEF01;
         uint address = 0xFFFFFFFF;
         SerialPort serial = new SerialPort();
 
         //constructors for fingerSensors, also makes a SerialPort useful
-        public fingerSensor(string port, int baud, uint addr)
+        public FingerSensor(string port, int baud, uint addr)
         {
             address = addr;
             serial.BaudRate = baud;
             serial.PortName = port;
+            serial.Open(); ///to open port
         }
-        public fingerSensor(string port, int baud)
+        public FingerSensor(string port, int baud)
         {
             serial.BaudRate = baud;
             serial.PortName = port;
+
+            serial.Open(); ///to open port
+            serial.DiscardInBuffer();
+            serial.DiscardOutBuffer();
         }
-        public fingerSensor()
+        public FingerSensor(string port)
+        {
+            serial.BaudRate = 57600; //default baud rate for scanner
+            serial.PortName = port;
+            serial.Open(); ///to open port
+        }
+        public FingerSensor()
         {
             serial.BaudRate = 1; //for testing
         }
 
         //handshake command
-        public void handshake()
+        public bool handshake()
         {
             byte[] contents = {0x17,0x00};
             write((uint)0x01, contents);
+
+            /////////////////wait
+            int j = 0;
+            for (int i = 0; i < 500; i++)
+            {
+                j++;
+            }
+            /////////////
+
+            return read();
         }
 
         //writes the command to the fingerprint scanner, requires the package identifier and the instruction packets
@@ -41,8 +62,8 @@ namespace Fingerprint
         {
             // turn total packet into a byte array, then send with Serial.Write(Byte[],Int32,Int32)
             uint length = (uint)packets.GetLength(0) + 2; //length is the packet length plus the checksum(2 bytes)
-            uint total_length = length + 9; //length+header(2)+address(4)+length of length(2)+pid(1)
-            byte[] command = new byte[total_length]; //make packet to send to scanner
+            uint totalLength = length + 9; //length+header(2)+address(4)+length of length(2)+pid(1)
+            byte[] command = new byte[totalLength]; //make packet to send to scanner
             uint checksum = pid + length; //calculate the checksum
             for (int i = 0; i < packets.GetLength(0); i++)
             {
@@ -59,24 +80,70 @@ namespace Fingerprint
             command[6] = (byte)pid;
             command[7] = (byte)(length >> 8);
             command[8] = (byte)length;
-            for (int i = 9; i < total_length-2; i++)
+            for (int i = 9; i < totalLength-2; i++)
             {
                 command[i] = packets[i - 9];
             }
-            command[total_length - 2] = (byte)(checksum >> 8);
-            command[total_length - 1] = (byte)checksum;
+            command[totalLength - 2] = (byte)(checksum >> 8);
+            command[totalLength - 1] = (byte)checksum;
             //write to serial
             if(serial.BaudRate > 1) //also test code
-                serial.Write(command, 0, (int)total_length);
+                serial.Write(command, 0, (int)totalLength);
 
             //test code
-            for (int i = 0; i < total_length; i++)
+            for (int i = 0; i < totalLength; i++)
             {
                 if (command[i] < 16) //nicer to read
                     Console.Write(0);
                 Console.Write(command[i].ToString("X"));
             }
+            Console.WriteLine(" ...end of write");
 
-        }
+        }//end write
+
+
+        bool read()
+        {
+            byte[] headLength = new byte[9];
+
+            while (serial.BytesToRead < 9)
+            {
+            }
+
+            serial.Read(headLength, 0, 9);
+            if ((int)headLength[8] < 1)
+                return false;//non-positive package length
+            byte[] contents = new byte[(int)headLength[8]];
+            //Console.WriteLine("length: " + (int)headLength[8]);
+            serial.Read(contents, 0, (int)headLength[8]);
+            ushort checksum = (ushort)((ushort)headLength[6] + (ushort)((ushort)headLength[7] << 8) + (ushort)headLength[8]);
+            for (int i = 0; i < (int)headLength[8]-2; i++)
+            {
+                checksum += (ushort)contents[i];
+            }
+
+            //test code//////////////////////////////////
+            for (int i = 0; i < 9; i++)
+            {
+                if (headLength[i] < 16) //nicer to read
+                    Console.Write(0);
+                Console.Write(headLength[i].ToString("X"));
+            }
+            for (int i = 0; i < headLength[8]; i++)
+            {
+                if (contents[i] < 16) //nicer to read
+                    Console.Write(0);
+                Console.Write(contents[i].ToString("X"));
+            }
+            Console.WriteLine(" ...end of read");
+            //end test///////////////////////////////////
+
+            if (checksum != (ushort)((ushort)((ushort)contents[headLength[8] - 2] << 8) + (ushort)contents[headLength[8] - 1]))
+            {
+                Console.WriteLine("Either the checksums don't agree or your math is wrong");
+                return false;
+            }
+            return true;
+        }//end read
     }
 }
