c19b39bd3fc4ece755b4bf6b7a1c4c60e53a7aca
itstomis <thomas@watanabe.ca>
2013-03-13 15:12:08 -0700

    Updated to reflect new panelboard design + ldsense

--- Powerpack_Controller/Motor_Controller.cpp
+++ Powerpack_Controller/Motor_Controller.cpp
@@ -1,17 +1,22 @@
 #include "Motor_Controller.h"
 
-MotorController::MotorController(int SensorPin, int MotorPin, int SetpointPin, int KpPin, int KiPin, int KdPin, double Kp, double Ki, double Kd, double dt, double Input, double Output, double Setpoint)
+MotorController::MotorController(int ModePin, int InputPressurePin, int MotorPin, int LoadSensePressurePin, int TuningBoxPin, int KpPin, int KiPin, int KdPin, double Kp, double Ki, double Kd, double dt, double LoadSenseOffset, double NumericalSetpoint, double Input, double Output, double Setpoint)
 : mPIDControl(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT),
-mSensorPin(SensorPin),
+mModePin(ModePin),
+mInputPressurePin(InputPressurePin),
 mMotorPin(MotorPin),
 mSetpointPin(SetpointPin),
+mLoadSensePressurePin(LoadSensePressurePin),
+mTuningBoxPin(TuningBoxPin),
 mKpPin(KpPin),
 mKiPin(KiPin),
 mKdPin(KdPin),
 mKp(Kp),
 mKi(Ki),
 mKd(Kd),
-mdt(dt), 
+mdt(dt),
+mLoadSenseOffset(LoadSenseOffset),
+mNumericalSetpoint(NumericalSetpoint),
 mInput(Input),
 mSetpoint(Setpoint),
 mOutput(Output){}
@@ -27,28 +32,49 @@ void MotorController::Initialize()
 {
     mPIDControl.SetMode(AUTOMATIC);
     mPIDControl.SetSampleTime(mdt);
+    mPIDControl.SetTunings(mKp, mKi, mKd);
 }
 
 void MotorController::Iterate()
 {
-  mKp = analogRead(mKpPin)*PID_Pot_Sensitivity;
-  mKi = analogRead(mKiPin)*PID_Pot_Sensitivity;
-  mKd = analogRead(mKdPin)*PID_Pot_Sensitivity;
-  mPIDControl.SetTunings(mKp, mKi, mKd);
+  if(digitalRead(mTuningBoxPin) == HIGH)
+  {
+    mKp = analogRead(mKpPin)*PID_Pot_Sensitivity;
+    mKi = analogRead(mKiPin)*PID_Pot_Sensitivity;
+    mKd = analogRead(mKdPin)*PID_Pot_Sensitivity;
+    mPIDControl.SetTunings(mKp, mKi, mKd);
+  }
   
-  mInput = GetPressure();
-  mSetpoint = analogRead(mSetpointPin)*Setpoint_Pot_Sensitivity;
-  
+  if(digitalRead(ModePin) == HIGH)
+  {
+    mSetpoint = GetPressure(LoadSensePressurePin) + mLoadSenseOffset;
+  }
+  if(digitalRead(ModePin) == LOW)
+  {
+    mSetpoint = mNumericalSetpoint;
+  }
+
+  mInput = GetPressure(InputPressurePin);
   mPIDControl.Compute();
   analogWrite(mMotorPin, mOutput);
 }
 
-double MotorController::GetPressure()
+double MotorController::GetPressure(int Pin)
 {
   int volt_sum = 0;
   for(int i=0; i<int(Pressure_Sample_Count); i++){
-    volt_sum = volt_sum + analogRead(mSensorPin);
+    volt_sum = volt_sum + analogRead(Pin);
   }
   
   return (volt_sum/Pressure_Sample_Count)*Pressure_Sensitivity*Analog_to_Voltage-Pressure_Intercept;
+}
+
+void SetLoadSenseOffset(double NewOffset)
+{
+  mLoadSenseOffset = NewOffset;
+}
+
+void SetNumericalSetpoint(double NewSetpoint)
+{
+  mNumericalSetpoint = NewSetpoint;
 }
