c80653a20030cc2c449ed8f7c89cc701fcdaa06e
itstomis <thomas@watanabe.ca>
2013-03-13 14:50:18 -0700

    added hard interrupt iteration timing

--- Powerpack_Controller/Dat_Control.ino
+++ Powerpack_Controller/Dat_Control.ino
@@ -7,6 +7,7 @@ Acknowledgements: PID Library by Brett Beauregard
 
 #include <PID_v1.h>
 #include <Motor_Controller.h>
+#include <MsTimer2.h>
 
 MotorController MyController(0, 10, 2, 3, 4, 5, 0, 0, 0, 10, 0, 0, 0);
 
@@ -16,10 +17,18 @@ void setup() {
   Serial.println("PID Code");
 
   delay(2000);
-
+  
   MyController.Initialize();
+  MsTimer2::set(100, Control_10Hz);
+  MsTimer2::start()
 }
 
-void loop() { //will replace loop() with more robust 10Hz interrupt later
+void Control_10Hz()
+{
   MyController.Iterate();
+}
+
+void loop() 
+{
+  //button and serial write stuff
 }
