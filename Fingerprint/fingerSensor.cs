2b8e1b405478ec94e78cc7370ff42996547dc3d5
Francesco Anzovino <francescoanzo@gmail.com>
2013-03-13 17:00:37 -0700

    Corrected Read and added address change

--- Fingerprint/fingerSensor.cs
+++ Fingerprint/fingerSensor.cs
@@ -9,7 +9,7 @@ namespace Fingerprint
     public class FingerSensor
     {
         ushort HEADER = 0xEF01;
-        uint address = 0xFFFFFFFF;
+        public uint address = 0xFFFFFFFF;
         SerialPort serial = new SerialPort();
 
         //constructors for fingerSensors, also makes a SerialPort useful
@@ -41,30 +41,53 @@ namespace Fingerprint
         }
 
         //handshake command
-        public bool handshake()
+        public bool Handshake()
         {
             byte[] contents = {0x17,0x00};
-            write((uint)0x01, contents);
+            Write(0x01, contents);
 
-            /////////////////wait
-            int j = 0;
-            for (int i = 0; i < 500; i++)
+            List<byte> packet = Read();
+            //test code
+            Console.WriteLine("Read: ");
+            foreach(byte thing in packet)
             {
-                j++;
+                Console.Write(thing.ToString("X")+",");
             }
-            /////////////
+            Console.Write("\n");
+            if (packet[9] == (byte)0)//end test code
+            //if (read()[9] == (byte)0)
+                return true;
+            else
+                return false;
+        }
 
-            return read();
+        //Used to set the address of the scanner
+        public bool SetAddr(uint newAddr)
+        {
+            byte[] contents = { 0x15, (byte)(newAddr >> 24), (byte)(newAddr >> 16), (byte)(newAddr >> 8), (byte)newAddr };
+            Write(0x01, contents);
+            List<byte> packet = Read();
+            uint readAddr = (uint)packet[5] + ((uint)packet[4] << 8) + ((uint)packet[3] << 16) + ((uint)packet[2] << 24);
+            Console.WriteLine("Input Address: {0}", newAddr.ToString("X"));
+            Console.WriteLine("Read Address: {0}", readAddr.ToString("X"));
+
+            if (newAddr == readAddr)
+            {
+                address = newAddr;
+                return true;
+            }
+            else
+                return false;
         }
 
         //writes the command to the fingerprint scanner, requires the package identifier and the instruction packets
-        void write(uint pid, byte[] packets)
+        private void Write(byte pid, byte[] packets)
         {
             // turn total packet into a byte array, then send with Serial.Write(Byte[],Int32,Int32)
             uint length = (uint)packets.GetLength(0) + 2; //length is the packet length plus the checksum(2 bytes)
             uint totalLength = length + 9; //length+header(2)+address(4)+length of length(2)+pid(1)
             byte[] command = new byte[totalLength]; //make packet to send to scanner
-            uint checksum = pid + length; //calculate the checksum
+            uint checksum = (uint)pid + length; //calculate the checksum
             for (int i = 0; i < packets.GetLength(0); i++)
             {
                 checksum += (uint)packets[i]; //value depends on value & number of packets
@@ -98,23 +121,36 @@ namespace Fingerprint
                 Console.Write(command[i].ToString("X"));
             }
             Console.WriteLine(" ...end of write");
-
+            //
         }//end write
 
-
-        bool read()
+        //returns list containing the packet contents
+        private List<byte> Read()
         {
             byte[] headLength = new byte[9];
+            List<byte> packet = new List<byte>(10);
 
             while (serial.BytesToRead < 9)
             {
             }
 
+            //read up to Package Length
             serial.Read(headLength, 0, 9);
             if ((int)headLength[8] < 1)
-                return false;//non-positive package length
+            {
+                packet[9] = 1;//non-positive package length
+                return packet;
+            }
+            for (int i = 0; i < headLength.Length; i++)
+            {
+                packet.Add(headLength[i]);
+            }
             byte[] contents = new byte[(int)headLength[8]];
             //Console.WriteLine("length: " + (int)headLength[8]);
+            while (serial.BytesToRead < headLength[8])
+            {
+            }
+            //read rest of package
             serial.Read(contents, 0, (int)headLength[8]);
             ushort checksum = (ushort)((ushort)headLength[6] + (ushort)((ushort)headLength[7] << 8) + (ushort)headLength[8]);
             for (int i = 0; i < (int)headLength[8]-2; i++)
@@ -141,9 +177,14 @@ namespace Fingerprint
             if (checksum != (ushort)((ushort)((ushort)contents[headLength[8] - 2] << 8) + (ushort)contents[headLength[8] - 1]))
             {
                 Console.WriteLine("Either the checksums don't agree or your math is wrong");
-                return false;
+                packet[9] = 1;
+                return packet;
             }
-            return true;
+            for (int i = 0; i < contents.Length; i++)
+            {
+                packet.Add(contents[i]);
+            }
+            return packet;
         }//end read
     }
 }
